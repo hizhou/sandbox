0d213ef9a6594d59790dfddfc7354ba6cb6167ac
hizhou <zhoujun.g@gmail.com>
2013-04-29 10:06:45 +0800

    add a js example for testing load-order and execute-order

---
+++ javascript/execute_order/script.php
@@ -0,0 +1,24 @@
+<?php
+class Request {
+	public function getQuery($key, $default = null) {
+		return isset($_GET[$key]) ? $_GET[$key] : $default;
+	}
+	public function getPost($key, $default = null) {
+		return isset($_POST[$key]) ? $_POST[$key] : $default;
+	}
+}
+$request = new Request();
+
+$action = $request->getQuery('action');
+if ($action == 'print') {
+	$sleep = $request->getQuery('sleep', 0);
+	$content = $request->getQuery('content', 'nothing');
+	
+	if ($sleep > 0) sleep($sleep);
+	
+	echo "document.write('$content <br>')";
+} elseif ($action == 'code') {
+	$content = $request->getQuery('content');
+	
+	echo $content;
+}
